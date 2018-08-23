0b3496e17dac9eb63810254405d75ed088b74d49
Chelsea Thiel-Jones <chelseatj3@gmail.com>
2018-08-23 15:24:16 -0400

    Initial Commit

---
+++ alarm/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  alarm
+//
+//  Created by Chelsea Thiel-Jones on 2018-08-23.
+//  Copyright © 2018 chelseatj3. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
